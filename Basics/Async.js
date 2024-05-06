4e441f5f9ce7d3de9c01818fd23b2feafd72ef40
Manan Pujara <104253184+MananPoojara@users.noreply.github.com>
2024-05-06 14:40:42 +0530

    update commit

---
+++ Basics/Async.js
@@ -0,0 +1,11 @@
+
+// Synchronous running one by one 
+
+var sum = (a, b) => {
+    return a + b;
+}
+console.log(sum(3, 4));//7
+console.log(sum(3, 4));//7
+console.log(sum(3, 4));//7
+console.log(sum(3, 4));//7
+console.log(sum(3, 4));//7
--- Basics/Callbacks.js
+++ Basics/Callbacks.js
@@ -12,3 +12,43 @@ var sum = (n1, n2, myCallback) => {
 sum(3, 4, Displaysum)
 
 // here we have one func is display some data and one function sum two number so as a example if we have some function take req data and some function add logic or read the req data and then response to user we can use callback so we don't have to call two function pass the param as a parameter to function 
+
+
+
+
+// Example we are checking user is auth or not so i have two func one where user giving input one for checking the validation and now so userInfo collect the users info and now the checkuser() check the validation so no need to call both func just use callback 
+
+var userInfo = (uname, passwd, Callbackfn) => {
+
+    console.log("Enter the User name : " + uname)
+    console.log("Enter the Password: " + passwd)
+    Callbackfn(uname, passwd);
+}
+
+var checkUser = (name, pass) => {
+
+    var allowedChars = "abcdefghijklmnopqrstuvwxyz1234567890";
+    let authName = false;
+    let authPass = false;
+
+    for (let char of allowedChars) {
+        if (name.includes(char)) {
+            authName = true;
+        }
+        if (pass.includes(char)) {
+            authPass = true;
+        }
+    }
+
+    if (authName && authPass && pass.length >= 8) {
+        console.log("Authenticate User")
+    }
+    else {
+        console.log("Not Authenticate User")
+
+    }
+
+}
+
+
+userInfo("xyz", "123478", checkUser)

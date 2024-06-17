82ed816fb6e73866129ba9c0afc3156b88009c95
MananPoojara <billybutcher215@gmail.com>
2024-06-17 20:29:43 +0530

    init commit

--- Basics/Map.js
+++ Basics/Map.js
@@ -23,5 +23,38 @@ function initMap() {
     return map;
 }
 
+function uniquepair() {
+    const map = new Map();
+    const obj = { name: 'Manan' }
+    const unique = map.set(obj, { age: 19 });
+    map.set(obj, { lname: 'pujara' });
+    map.set({ name: 'Manan' }, { fname: 'Manan' })
+    return map, unique;
+    map.ke
+
+}
+
 console.log(basicmap());
 console.log(initMap());
+console.log(uniquepair());
+
+
+function mapInfo() {
+    const map = new Map();
+    map.set('a', 2);
+    map.set({ v: 4, j: 5 }, 5)
+    map.set(() => {
+        console.log("hello word")
+    }, 2)
+
+    return {
+        mapsize: map.size,
+        maphas: map.has(5),
+        keysize: Object.keys(map),
+        values: map.values(),
+
+    }
+}
+
+
+console.log(mapInfo());

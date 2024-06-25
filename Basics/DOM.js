3ef65b7631a9b7fb9ae7a01593d85affe89a50b7
MananPoojara <billybutcher215@gmail.com>
2024-06-25 10:58:13 +0530

    init commit

--- Basics/DOM.js
+++ Basics/DOM.js
@@ -5,3 +5,24 @@ let welcomemsg = document.getElementById("welcome-msg");
 welcomemsg.textContent += username === "" ? `Guest` : username;
 
 
+const myh1 = document.createElement('h4');
+const space = document.createElement('br');
+
+
+myh1.innerText = "Hello writing from js using document.createElement()"
+
+document.body.appendChild(space)
+document.body.appendChild(space)
+document.body.appendChild(space)
+document.body.append("i append the text")
+document.body.append("DOM is Just api that have multile objects functions and manymore so document is our page and body has all the content using fucntion we can manupilate the document")
+document.body.appendChild(myh1)
+
+// document.onclick = () => {
+//     document.body.append("i append the text")
+// }
+
+//when we click on page anywhere the text apper 
+
+
+

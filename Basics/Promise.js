9fe212cfc85eb778b145883fc5512a4c1228ef67
MananPoojara <billybutcher215@gmail.com>
2024-07-10 23:02:26 +0530

    init commit

---
+++ Basics/Promise.js
@@ -0,0 +1,7 @@
+// first we see Fetch() and then we can see how and why to use promise()
+
+// fetch("https://smthing.com").then().catch().finally(); 
+// always finally called so now see promise 
+
+
+
